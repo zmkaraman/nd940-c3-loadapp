08b5e6d5a45856012aa30fd3f92a9c36dc5bf60d
tczkaraman <zmervekaraman@gmail.com>
2021-03-14 16:31:57 +0300

    arc fix

--- starter/app/src/main/java/com/udacity/LoadingButton.kt
+++ starter/app/src/main/java/com/udacity/LoadingButton.kt
@@ -17,7 +17,7 @@ class LoadingButton @JvmOverloads constructor(
     private var mRight = 0F
 
     private var downloadStatus : ButtonState = ButtonState.Clicked  // The active selection.
-    private val cornerRadius = 10.0f
+    private val cornerRadius = 40.0f
 
 
     private var valueAnimator = ValueAnimator()
@@ -29,8 +29,6 @@ class LoadingButton @JvmOverloads constructor(
     private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
         style = Paint.Style.FILL
         textAlign = Paint.Align.CENTER
-        textSize = 55.0f
-        typeface = Typeface.create( "", Typeface.BOLD)
     }
 
     private val textPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
@@ -61,29 +59,15 @@ class LoadingButton @JvmOverloads constructor(
         var mOval = RectF()
 
 
-        val arcDiameter = cornerRadius * 2
-        val arcRectSize = measuredHeight.toFloat() - paddingBottom.toFloat() - arcDiameter
-
-
-
         if (bounds != null) {
 
             mRectangle = RectF(bounds.left.toFloat(),
                 bounds.top.toFloat(), 0F, bounds.bottom.toFloat())
 
-            mOval = RectF(paddingEnd + arcDiameter ,
-                paddingTop + arcDiameter, 150F, bounds.bottom.toFloat()
+            mOval = RectF(bounds.right.toFloat() - (cornerRadius * 2 + 150),
+                bounds.top.toFloat() + 50 ,
+                bounds.right.toFloat() - 150, bounds.bottom.toFloat() -50
             )
-
-/*
-            canvas?.drawArc(paddingEnd + arcDiameter ,
-                paddingTop + arcDiameter,
-                arcRectSize,
-                arcRectSize,
-                0f,
-                mRight/2,
-                true,
-                arcPaint)*/
 
             canvas?.drawRect(mRectangle, paint)
 
@@ -93,6 +77,8 @@ class LoadingButton @JvmOverloads constructor(
             ButtonState.Clicked -> {
                 mRight = 0f
                 textPaint.color = Color.WHITE
+
+                canvas?.drawText(downloadStatus.getLabel(), (width/2).toFloat(), (height/2).toFloat(), textPaint)
             }
             ButtonState.Loading -> {
                 textPaint.color = Color.LTGRAY
@@ -111,14 +97,7 @@ class LoadingButton @JvmOverloads constructor(
                     }
                 }
 
-              /*  canvas?.drawArc(paddingEnd + arcDiameter ,
-                    paddingTop + arcDiameter,
-                    arcRectSize,
-                    arcRectSize,
-                    0f,
-                    mRight/2,
-                    true,
-                    arcPaint)*/
+                canvas?.drawText(downloadStatus.getLabel(), (width/2).toFloat(), (height/2).toFloat(), textPaint)
 
                 canvas?.drawArc( mOval, 0f,
                     mRight/2,
@@ -128,10 +107,10 @@ class LoadingButton @JvmOverloads constructor(
             ButtonState.Completed -> {
                 textPaint.color = Color.GREEN
 
+                canvas?.drawText(downloadStatus.getLabel(), (width/2).toFloat(), (height/2).toFloat(), textPaint)
             }
         }
 
-        canvas?.drawText(downloadStatus.getLabel(), (width/2).toFloat(), (height/2).toFloat(), textPaint)
     }
 
 
