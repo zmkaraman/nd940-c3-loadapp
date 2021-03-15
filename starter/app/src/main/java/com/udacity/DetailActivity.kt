3e8f8998c4f51d496719f89470ead64204d4cf58
tczkaraman <zmervekaraman@gmail.com>
2021-03-16 01:41:41 +0300

    code-review fix#6 dissmiss notification when detail screen opens

--- starter/app/src/main/java/com/udacity/DetailActivity.kt
+++ starter/app/src/main/java/com/udacity/DetailActivity.kt
@@ -1,7 +1,9 @@
 package com.udacity
 
+import android.app.NotificationManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_detail.*
 import kotlinx.android.synthetic.main.content_detail.*
 
@@ -11,6 +13,13 @@ class DetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
         setSupportActionBar(toolbar)
+
+        val notificationManager =
+            ContextCompat.getSystemService(
+                this,
+                NotificationManager::class.java
+            ) as NotificationManager
+        notificationManager.cancelNotifications()
 
 
         textViewFileName.text = intent.getStringExtra("name")
